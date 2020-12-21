540a327e2190be50bed555229982f8af2cb3a875
Aaron Yu <aaron@meto.com.tw>
2020-12-22 07:26:25 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+)
---
+++ gorm-5.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var db *gorm.DB
+var err error
+
+type Person struct {
+	ID        uint   `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+func main() {
+
+	// NOTE: See we're using = to assign the global var
+	// instead of := which would assign it only in this function
+	db, err = gorm.Open("sqlite3", "./gorm.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Person{})
+
+	r := gin.Default()
+	r.GET("/people/", GetPeople)
+	r.GET("/people/:id", GetPerson)
+	r.POST("/people", CreatePerson)
+
+	r.Run(":8080")
+}
+
+func CreatePerson(c *gin.Context) {
+
+	var person Person
+	//person = Person{FirstName: "Jane", LastName: "Smith"}
+	err := c.ShouldBindJSON(&person)
+	if err != nil {
+		fmt.Println("請求發生錯誤：")
+		c.JSON(http.StatusOK, gin.H{"msg": err.Error()})
+		return
+	}
+	body := c.Copy().Request.Body
+	s, _ := ioutil.ReadAll(body)
+	fmt.Println(string(s))
+	db.Create(&person)
+
+	c.JSON(200, person)
+}
+
+func GetPerson(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var person Person
+	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, person)
+	}
+}
+
+func GetPeople(c *gin.Context) {
+	var people []Person
+	if err := db.Find(&people).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, people)
+	}
+
+}
